847633ddae479298303c330e85c9f992920d7e52
PeppeGr <62112379+PeppeGr@users.noreply.github.com>
2020-04-02 18:31:21 +0200

    Update back2top.js

--- js/back2top.js
+++ js/back2top.js
@@ -1,7 +1,7 @@
 /*Scroll to top when arrow up clicked BEGIN*/
 $(window).scroll(function() {
     var height = $(window).scrollTop();
-    if (height > 100) {
+    if (height > 300) {
         $('#back2Top').fadeIn();
     } else {
         $('#back2Top').fadeOut();
