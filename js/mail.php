04ba92af4cc7b2cfe58e7b1bb24b57d73131d53b
PeppeGr <62112379+PeppeGr@users.noreply.github.com>
2020-06-19 19:18:08 +0200

    Update mail.php

--- js/mail.php
+++ js/mail.php
@@ -1,11 +1,11 @@
-<?php $name = $_POST['fname'];
-$lname = $_POST['lname'];
+<?php
+$miamail = "giuseppe9486@gmail.com";
+$successo = $_POST['successo'];
+$nome = $_POST['nome'];
 $email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "giuseppe9486@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "TGrazie!";
+$msg = $_POST['msg'];
+$messaggio = "Nome: " . $fname . "\nEmail: " . $email . "\nMessaggio: " . $message;
+$headers = "From: " . $miamail . "\r\n" . "Reply-To: " . $miamail;
+mail($miamail,"Messaggio dal sito",$messaggio,$headers);
+header("location: " . $successo);
 ?>
